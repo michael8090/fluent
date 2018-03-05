13706f4acabe493bd7fde52dca83734ca467e6eb
unknown <michael8090@gmail.com>
2018-03-06 01:40:57 +0800

    docs

--- my-app/src/flunent/GraphicsPrimitive.ts
+++
@@ -1,4 +0,0 @@
-export default class GraphicsPrimitive<T = {}> {
-  type: string;
-  data: T;
-}
--- my-app/src/flunent/utils/color.ts
+++
@@ -1,7 +0,0 @@
-export function hexToCss(hex: number) {
-  const css = hex.toString(16);
-  const { length } = css;
-  return (
-    '#' + (length < 6 ? '0'.repeat(6 - length) + css : css.substring(0, 6))
-  );
-}
